0d42ecdded7e26144ae08b5c309589bfcbc1e44e
Lasantha <dlasanthax@gmail.com>
2024-07-16 08:22:01 +0530

    only tests

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,16 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
     root: true,
-    rules: { '@typescript-eslint/no-floating-promises': 'error' },
+    rules: {
+        '@typescript-eslint/no-floating-promises': 'error',
+        'no-restricted-syntax': [
+            'error',
+            {
+                selector: "CallExpression[callee.property.name='only']",
+                message: '.only tests found',
+            },
+        ],
+    },
     parserOptions: {
         project: './tsconfig.json', // Path to your tsconfig.json file
     },
